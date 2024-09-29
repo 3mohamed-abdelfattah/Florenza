95d466069e56a8e59365092869bee943ea86c384
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-09-29 17:57:16 +0300

    Finish Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryText: "#121212",
+        secondaryText: "#808080",
+        primaryBackground: "#FFFFFF",
+      },
+      screens: {
+        '2xl': '1900px',
+        'xl': '1250px',
+        'lg': '950px',
+        'md': '850px',
+        'sm': '600px',
+      },
+    },
   },
   plugins: [],
 }
