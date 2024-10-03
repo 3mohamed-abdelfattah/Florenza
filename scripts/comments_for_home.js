750e34bcc29d3c1b0fc77fa0df8b4427c89696fb
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-03 19:09:11 +0300

    Users Authentication

--- scripts/comments_for_home.js
+++ scripts/comments_for_home.js
@@ -1,52 +1,53 @@
 var arrcommentName = [
     "Ronald Richards",
-      "Emily Clark",
-      "Michael Brown",
-      "Sarah Johnson",
-      "David Wilson"
+    "Emily Clark",
+    "Michael Brown",
+    "Sarah Johnson",
+    "David Wilson"
 ]
 var arrcommentText = [
     "Ordered flowers online and they were the best bouquet! Impressed everyone around. Highly recommend this flower shop!",
-      "The arrangements are stunning! The flowers were fresh, beautifully arranged, and delivered on time. Will definitely order again.",
-      "Amazing service! The bouquet looked even better in person than on the website. My wife loved it. Thank you!",
-      "I loved the unique mix of fresh and dried flowers in the bouquet I ordered. Great for any occasion. Highly recommended!",
-      "Excellent customer service and the quality of the flowers was exceptional. Will be using this shop for all future occasions."
+    "The arrangements are stunning! The flowers were fresh, beautifully arranged, and delivered on time. Will definitely order again.",
+    "Amazing service! The bouquet looked even better in person than on the website. My wife loved it. Thank you!",
+    "I loved the unique mix of fresh and dried flowers in the bouquet I ordered. Great for any occasion. Highly recommended!",
+    "Excellent customer service and the quality of the flowers was exceptional. Will be using this shop for all future occasions."
 ]
 var i = 0;
 setInterval(function () {
     i++
-    if (i>4) {
+    if (i > 4) {
         i = 0
-        document.getElementById("commentText").innerHTML=`“${arrcommentText[i]}”`
-        document.getElementById("commentName").innerHTML=`– ${arrcommentName[i]}`
+        document.getElementById("commentText").innerHTML = `“${arrcommentText[i]}”`
+        document.getElementById("commentName").innerHTML = `– ${arrcommentName[i]}`
 
     }
-    else{
-        document.getElementById("commentText").innerHTML=`“${arrcommentText[i]}”`
-        document.getElementById("commentName").innerHTML=`– ${arrcommentName[i]}`
+    else {
+        document.getElementById("commentText").innerHTML = `“${arrcommentText[i]}”`
+        document.getElementById("commentName").innerHTML = `– ${arrcommentName[i]}`
     }
-},5000)
-document.getElementById("Left-arrow-review").addEventListener("click",function () {
+}, 2000)
+document.getElementById("Left-arrow-review").addEventListener("click", function () {
     i++
-    if(i > 4){
+    if (i > 4) {
         i = 0
-        document.getElementById("commentText").innerHTML=`“${arrcommentText[i]}”`
-        document.getElementById("commentName").innerHTML=`– ${arrcommentName[i]}`
+        document.getElementById("commentText").innerHTML = `“${arrcommentText[i]}”`
+        document.getElementById("commentName").innerHTML = `– ${arrcommentName[i]}`
     }
-    else{
-        document.getElementById("commentText").innerHTML=`“${arrcommentText[i]}”`
-        document.getElementById("commentName").innerHTML=`– ${arrcommentName[i]}`
+    else {
+        document.getElementById("commentText").innerHTML = `“${arrcommentText[i]}”`
+        document.getElementById("commentName").innerHTML = `– ${arrcommentName[i]}`
     }
 })
-document.getElementById("Right-arrow-review").addEventListener("click",function () {
+document.getElementById("Right-arrow-review").addEventListener("click", function () {
+
     i--
-    if(i < 0){  
-        i = 4     
-        document.getElementById("commentText").innerHTML=`“${arrcommentText[i]}”`
-        document.getElementById("commentName").innerHTML=`– ${arrcommentName[i]}`
+    if (i < 0) {
+        i = 4
+        document.getElementById("commentText").innerHTML = `“${arrcommentText[i]}”`
+        document.getElementById("commentName").innerHTML = `– ${arrcommentName[i]}`
     }
-    else{
-        document.getElementById("commentText").innerHTML=`“${arrcommentText[i]}”`
-        document.getElementById("commentName").innerHTML=`– ${arrcommentName[i]}`
+    else {
+        document.getElementById("commentText").innerHTML = `“${arrcommentText[i]}”`
+        document.getElementById("commentName").innerHTML = `– ${arrcommentName[i]}`
     }
 })
--- scripts/sign.js
+++ scripts/sign.js
@@ -1,29 +1,78 @@
-var userchk = /^[a-zA-Z0-9_.]{6,20}$/
-var passchk = /^[a-zA-Z0-9_.]{6,}$/
+// Inputs Verification 
+var userCheck = /^[a-zA-Z0-9_.]{6,20}$/
+var passCheck = /^[a-zA-Z0-9_.]{6,}$/
 var username = document.forms[0][0]
 var password = document.forms[0][1]
-var invuser = document.getElementById("labuser")
-var invpass = document.getElementById("labpass")
-document.getElementById("btn").addEventListener('click',function (e) {
-    if (!userchk.test(username.value) && !passchk.test(password.value)) {
+var invalidUser = document.getElementById("labuser")
+var invalidPass = document.getElementById("labpass")
+document.getElementById("btn").addEventListener('click', function (e) {
+    if (!userCheck.test(username.value) && !passCheck.test(password.value)) {
         e.preventDefault()
-        invuser.innerHTML="invaild username"
-        invuser.style.color="red"
-        invpass.innerHTML="invaild password"
-        invpass.style.color="red"
+        invalidUser.innerHTML = "invalid username"
+        invalidUser.style.color = "red"
+        invalidPass.innerHTML = "invalid password"
+        invalidPass.style.color = "red"
     }
-    else if (!userchk.test(username.value) && passchk.test(password.value)) {
+    else if (!userCheck.test(username.value) && passCheck.test(password.value)) {
         e.preventDefault()
-        invuser.innerHTML="invaild username"
-        invuser.style.color="red"
-        invpass.innerHTML=""
-        invpass.style.color="white"
+        invalidUser.innerHTML = "invalid username"
+        invalidUser.style.color = "red"
+        invalidPass.innerHTML = ""
+        invalidPass.style.color = "white"
     }
-    else if (userchk.test(username.value) && !passchk.test(password.value)) {
+    else if (userCheck.test(username.value) && !passCheck.test(password.value)) {
         e.preventDefault()
-        invpass.innerHTML="invaild password"
-        invpass.style.color="red"
-        invuser.innerHTML=""
-        invuser.style.color="white"
+        invalidPass.innerHTML = "invalid password"
+        invalidPass.style.color = "red"
+        invalidUser.innerHTML = ""
+        invalidUser.style.color = "white"
     }
 })
+
+
+//  User Authentication
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('../data/ourUsers.json')
+        .then(response => response.json())
+        .then(data => {
+            const users = data.users;
+
+            const user = users.find(user => user.username === username && user.password === password);
+
+            if (user) {
+                localStorage.setItem('isLoggedIn', 'true');
+                // Toastify for Success
+                Toastify({
+                    text: "Login successful!",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top",
+                    position: "left",
+                    backgroundColor: "#627211b6",
+                }).showToast();
+
+                setTimeout(() => {
+                    window.location.href = '../index.html';
+                }, 3000);
+
+            } else {
+                // Toastify for Failure
+                Toastify({
+                    text: "Incorrect username or password.",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top",
+                    position: "left",
+                    backgroundColor: "#ff5f6d",
+                }).showToast();
+            }
+        })
+        .catch(error => {
+            console.error('Error loading users:', error);
+        });
+});
